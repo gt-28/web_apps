c19a12c587ba142474542450d6e279c52507eb0f
gt-28 <itejagadde@gmail.com>
2019-12-21 12:58:20 -0700

    updated script

--- deploy_tomcat_web.sh
+++ deploy_tomcat_web.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Install Tomcat
+echo "Installing tomcat"
+sudo yum install tomcat -y
+
 # Setup variables
-export apache2_PATH='/var/www/html/'
+export TOMCAT_PATH='/etc/tomcat/'
 export ARTIFACT_TMP_PATH='/home/jenkins/tmp_web_artifact'
 export ARTIFACT_NAME='web_artifact.tar.gz'
 
 # Remove tmp directory
 if [ -d ${ARTIFACT_TMP_PATH} ]; then
-rm -rf ${ARTIFACT_TMP_PATH}
+  rm -rf ${ARTIFACT_TMP_PATH}
 fi
 
 # Copy artifact to tmp dir and extract
@@ -15,29 +19,29 @@ mkdir -p ${ARTIFACT_TMP_PATH}
 cp ${ARTIFACT_NAME} ${ARTIFACT_TMP_PATH}
 cd ${ARTIFACT_TMP_PATH}
 echo "Extracting files:"
-ar xvzf ${ARTIFACT_NAME}
+tar xvzf ${ARTIFACT_NAME}
 
 # Remove env from config files
 for FILENAME in ${ARTIFACT_TMP_PATH}/web_artifact/config/*; do
-TRIMMED_FILENAME=$(echo ${FILENAME} | cut -d. -f1,2)
-mv ${FILENAME} ${TRIMMED_FILENAME}
+  TRIMMED_FILENAME=$(echo ${FILENAME} | cut -d. -f1,2)
+  mv ${FILENAME} ${TRIMMED_FILENAME}
 done
 
-# Stop apache27
-echo "Stopping apache2"
-sudo service apache2 stop
+# Stop TOMCAT7
+echo "Stopping TOMCAT"
+sudo service tomcat stop
 
 # Delete any existing deployments
 echo "Removing previous deployments"
-sudo rm -rf ${apache2_PATH}/*
+sudo rm -rf ${TOMCAT_PATH}/conf.d/*
 
 # Copy new war file and config to deployments directory
-sudo \cp ${ARTIFACT_TMP_PATH}/web_artifact/config/* ${apache2_PATH}/configuration/
-sudo \cp ${ARTIFACT_TMP_PATH}/web_artifact/war/*.war ${apache2_PATH}/deployments/
+sudo \cp ${ARTIFACT_TMP_PATH}/web_artifact/config/* ${TOMCAT_PATH}/conf.d/
+sudo \cp ${ARTIFACT_TMP_PATH}/web_artifact/war/*.war ${TOMCAT_PATH}/deployments/
 
 # Change permissions
-sudo chown -R apache2. ${apache2_PATH}/deployments/*
-sudo chown -R apache2. ${apache2_PATH}/configuration/*
+sudo chown -R tomcat. ${TOMCAT_PATH}/deployments/*
+sudo chown -R tomcat. ${TOMCAT_PATH}/conf.d/*
 
-# Start apache27
-sudo service apache2 start
+# Start TOMCAT7
+sudo service tomcat start
